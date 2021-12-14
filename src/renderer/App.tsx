c0b5ddfeef8c33b421e5d66dceca1bcd90484e5d
qingzii <qingzii@43.com>
2021-12-14 20:02:38 +0800

    first commit

---
+++ src/renderer/App.tsx
@@ -0,0 +1,25 @@
+import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.scss';
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+};
+export default App;
