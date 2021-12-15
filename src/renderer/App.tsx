5df764ecbedcca905a64f247702a634a919bd351
qingzii <qingzii@43.com>
2021-12-15 11:14:21 +0800

    ..

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,25 +1,25 @@
-import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.scss';
+import { MemoryRouter as Router, Switch, Route } from 'react-router-dom'
+import icon from '../../assets/icon.svg'
+import './App.scss'
 
 const Hello = () => {
   return (
     <div>
-                    <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
+                        <div className='Hellooo'>
+        <img width='200px' alt='icon' src={icon} />
       </div>
       <h1>electron-react-boilerplate</h1>
     </div>
-  );
-};
+  )
+}
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+        <Route path='/' component={Hello} />
       </Switch>
     </Router>
-  );
-};
-export default App;
+  )
+}
+export default App
